b15587211d6d7047175106f32e275efa2995a5a2
Teresa Knowles <teresa07moreno@gmail.com>
2019-06-03 12:42:16 -0600

    Parse through CSV and create a Team record for each unique Team

--- data/import_team.js
+++ data/import_team.js
@@ -1,2 +1,27 @@
+var csv = require('csv-parser');
 var fs = require('fs');
-var csv = require('csv')
+const Team = require('../models').Team
+const teams = {};
+const results = [];
+
+
+fs.createReadStream('./data/olympic_data_2016.csv')
+  .pipe(csv())
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    console.log(results[0].Team)
+    console.log(results[1].Team)
+    console.log(results[2].Team)
+    console.log(results[1].Team === results[2].Team)
+    var i;
+    for (i=0; i < results.length; i++) {
+      Team.findOrCreate({
+        where: {
+          name: results[i].Team
+        }
+      })
+      .then(team => {
+        console.log(team)
+      })
+    }
+  });
