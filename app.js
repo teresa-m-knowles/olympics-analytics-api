5f9578a3ef9e3d56294781fbcb06b13b64d804c9
Teresa Knowles <teresa07moreno@gmail.com>
2019-06-04 20:00:25 -0600

    Setup Graphql

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema/schema');
+
+
 var indexRouter = require('./routes/index');
 var olympiansRouter = require('./routes/api/v1/olympians');
 var statsRouter = require('./routes/api/v1/stats');
@@ -20,5 +24,10 @@ app.use('/', indexRouter);
 app.use('/api/v1/olympians', olympiansRouter)
 app.use('/api/v1/olympian_stats', statsRouter)
 app.use('/api/v1/events', eventsRouter)
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
 
 module.exports = app;
